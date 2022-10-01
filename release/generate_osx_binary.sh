819b2cbd1642c7450377f75cbe06c41401c7da32
chriscado <cdoman@cadosecurity.com>
2022-10-01 13:31:51 +0100

    Try fixing fat binary

--- release/generate_osx_binary.sh
+++ release/generate_osx_binary.sh
@@ -1,4 +1,13 @@
 # Tested with python 3.10 and PyInstaller 5.4.1
 
-pip3 install -r requirements.txt
-pyinstaller  --onefile --clean --target-arch universal2 varc.py
+PATHS="../venv/lib/python3.10/site-packages"
+
+python3 -m venv venv
+source ./venv/bin/activate
+cd cado-host-python
+
+
+python3 -m pip install --upgrade pip
+python3 -m pip install pyinstaller==4.10
+python3 -m pip install -r requirements.txt
+python3 -m PyInstaller  --onefile --clean --paths $PATHS --target-arch universal2 varc.py
